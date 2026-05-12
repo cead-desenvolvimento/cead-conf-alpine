0d48ee3082c30944f89deec220c199feb2bae2cf
Rodrigo Marangon <desenvolvimento.cead@ufjf.br>
2026-05-12 10:07:19 -0300

    Update conf nftables

--- scripts/copiar_conf_fail2ban.sh
+++ scripts/copiar_conf_fail2ban.sh
@@ -5,5 +5,7 @@ PATH=/usr/bin:/bin
 
 if [ ! -f "/etc/fail2ban" ]; then
     cp /srv/cead-conf-alpine/fail2ban/jail.local /etc/fail2ban/
+    cp /srv/cead-conf-alpine/fail2ban/action.d/*.conf /etc/fail2ban/action.d/
     cp /srv/cead-conf-alpine/fail2ban/filter.d/*.conf /etc/fail2ban/filter.d/
+    cp /srv/cead-conf-alpine/fail2ban/nftables.nft /etc/
 fi
